dcbde2b332b960db96982a2f3b1bd33371fbc44f
pUrGe12 <helloitsme0218@gmail.com>
2025-05-24 21:54:45 +0530

    fixed this up, only new_scan_task works well

--- nettacker/core/tasks.py
+++ nettacker/core/tasks.py
@@ -19,9 +19,16 @@ def new_scan_task(form_values):
 	nettacker_app = Nettacker(api_arguments = SimpleNamespace(**form_values))
 	app_arguments = SimpleNamespace(**form_values)
 
+<<<<<<< HEAD
 	# Bruh till here everything's fine!
 	if not isinstance(app_arguments.selected_modules, list):
 		app_arguments.selected_modules = app_arguments.selected_modules.split(",")			# make sure we don't come here if we have actually selected more.
+=======
+	print("These are the arguments to nettacker: {}".format(app_arguments))			# Is it splitting en.wikipedia.org
+	# Bruh till here everything's fine!
+	if not isinstance(app_arguments.selected_modules, list):
+		app_arguments.selected_modules = [app_arguments.selected_modules]			# make sure we don't come here if we have actually selected more.
+>>>>>>> 8b7c9850 (fixed this up, only new_scan_task works well)
 	nettacker_app.arguments = app_arguments
 	nettacker_app.run()
 
